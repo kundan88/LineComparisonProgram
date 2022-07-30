9a91479bd9a3fff8e86fa74d74fc1a38db948759
Kundan <kundan kamble>
2022-07-29 19:38:52 -0700

    Added Lenth Of Line program

--- LineComparison/LineComparison.cs
+++ LineComparison/LineComparison.cs
@@ -8,9 +8,20 @@ namespace LineComparison
 {
     class Program
     {
-        public static void DisplayMsg()
+        public static void LengthOfLine()
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("Welcome to Line Comparision Program");
+            Console.WriteLine();
+            Console.WriteLine("Enter the coordinates: ");
+
+            int x1 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+
+            double line1 = Math.Sqrt((Math.Pow((x2 - x1), 2)) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine(" length Of Line is: " + Math.Round(line1, 3));
+
         }
     }
 }
