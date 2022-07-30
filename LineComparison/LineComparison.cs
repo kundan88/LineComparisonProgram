ac5d53f89f681d73e30ae3eca9fe37421491eba1
Kundan <kundan kamble>
2022-07-29 19:30:51 -0700

    Added Welcome to Line comparison program

---
+++ LineComparison/LineComparison.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    class Program
+    {
+        public static void DisplayMsg()
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
---
+++ LineComparison/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LineComparison
+{
+   class program
+    {
+        public static void Main(string[] args)
+        {
+            Program.DisplayMsg();
+        }
+    }
+}
