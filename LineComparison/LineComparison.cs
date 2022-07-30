1a4bb421a5f5352afed2bc79f2f6a0a9abb0eb02
Kundan <kundan kamble>
2022-07-30 13:54:19 -0700

    Added Line is equal or Greater program

--- LineComparison/LineComparison.cs
+++ LineComparison/LineComparison.cs
@@ -8,9 +8,36 @@ namespace LineComparison
 {
     class Program
     {
-        public static void DisplayMsg()
-        {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
-        }
-    }
+        public static void LineIsGreaterOrEqual()
+		{
+
+			int X, Y;
+			Console.Write("Compare Line\n");
+			Console.Write("-----------------------");
+			Console.Write("\n\n");
+
+			Console.Write("Enter the value for X coordinate :");
+			X = Convert.ToInt16(Console.ReadLine());
+			Console.Write("Enter the value for Y coordinate :");
+			Y = Convert.ToInt16(Console.ReadLine());
+
+			//Compare Method
+			int compare_val = X.CompareTo(Y);
+			Console.WriteLine(compare_val);
+
+			if (compare_val == 0)
+			{
+				Console.WriteLine("Both Lines are equal");
+			}
+			else if (compare_val < 0)
+			{
+				Console.WriteLine("X is less than Y");
+			}
+			else if (compare_val > 0)
+			{
+				Console.WriteLine("X is Greater than Y");
+			}
+		}
+	}
 }
+
